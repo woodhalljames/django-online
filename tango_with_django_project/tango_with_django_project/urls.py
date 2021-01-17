a57dec507dc4ea95d444e0ce0d3c661a306ea5cd
James <woodhalljames@gmail.com>
2021-01-17 12:38:51 -0500

    added urls.py to master

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include 
 from rango import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # the above maps any urls starting with rango/ to be handled by rango app.
     path('admin/', admin.site.urls),
 ]
